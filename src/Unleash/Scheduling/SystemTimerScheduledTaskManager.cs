0085aadd6625902be21167fb2c486825aed4c1d2
Mohammad Amin Pourmoradian <46063083+MohammadAminPourmoradian@users.noreply.github.com>
2025-10-07 13:28:13 +0330

    Fix background tasks leak when CreateClientAsync fails (issue #304)

--- src/Unleash/Scheduling/SystemTimerScheduledTaskManager.cs
+++ src/Unleash/Scheduling/SystemTimerScheduledTaskManager.cs
@@ -7,107 +7,111 @@ using Unleash.Logging;
 
 namespace Unleash.Scheduling
 {
-    /// <inheritdoc />
-    /// <summary>
-    /// Default task manager based on System.Threading.Timers.
-    /// </summary>
-    internal class SystemTimerScheduledTaskManager : IUnleashScheduledTaskManager
-    {
-        private static readonly ILog Logger = LogProvider.GetLogger(typeof(SystemTimerScheduledTaskManager));
+	/// <inheritdoc />
+	/// <summary>
+	/// Default task manager based on System.Threading.Timers.
+	/// </summary>
+	internal class SystemTimerScheduledTaskManager : IUnleashScheduledTaskManager
+	{
+		private static readonly ILog Logger = LogProvider.GetLogger(typeof(SystemTimerScheduledTaskManager));
 
-        private readonly Dictionary<string, Timer> timers = new Dictionary<string, Timer>();
+		private readonly Dictionary<string, Timer> timers = new Dictionary<string, Timer>();
 
-        public void Configure(IEnumerable<IUnleashScheduledTask> tasks, CancellationToken cancellationToken)
-        {
-            foreach (var task in tasks)
-            {
-                ConfigureTask(task, cancellationToken);
-            }
-        }
+		public void Configure(IEnumerable<IUnleashScheduledTask> tasks, CancellationToken cancellationToken)
+		{
+			foreach (var task in tasks)
+			{
+				ConfigureTask(task, cancellationToken);
+			}
+		}
 
-        private void ConfigureTask(IUnleashScheduledTask task, CancellationToken cancellationToken)
-        {
-            var name = task.Name;
+		private void ConfigureTask(IUnleashScheduledTask task, CancellationToken cancellationToken)
+		{
+			var name = task.Name;
 
-            async void Callback(object state)
-            {
-                try
-                {
-                    if (!cancellationToken.IsCancellationRequested)
-                    {
-                        await task.ExecuteAsync(cancellationToken);
-                    }
-                }
-                catch (TaskCanceledException taskCanceledException)
-                {
-                    if (!cancellationToken.IsCancellationRequested)
-                    {
-                        Logger.Warn(() => $"UNLEASH: Task '{name}' cancelled ...", taskCanceledException);
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Logger.Warn(() => $"UNLEASH: Unhandled exception from background task '{name}'.", ex);
-                }
-                finally
-                {
-                    if (cancellationToken.IsCancellationRequested)
-                    {
-                        // Stop the timer.
-                        if (timers.ContainsKey(name))
-                        {
-                            timers[name].SafeTimerChange(Timeout.Infinite, Timeout.Infinite, ref disposeEnded);
-                        }
-                    }
-                }
-            }
+			async void Callback(object state)
+			{
+				try
+				{
+					if (!cancellationToken.IsCancellationRequested)
+					{
+						await task.ExecuteAsync(cancellationToken);
+					}
+				}
+				catch (TaskCanceledException taskCanceledException)
+				{
+					if (!cancellationToken.IsCancellationRequested)
+					{
+						Logger.Warn(() => $"UNLEASH: Task '{name}' cancelled ...", taskCanceledException);
+					}
+				}
+				catch (Exception ex)
+				{
+					Logger.Warn(() => $"UNLEASH: Unhandled exception from background task '{name}'.", ex);
+				}
+				finally
+				{
+					if (cancellationToken.IsCancellationRequested)
+					{
+						// Stop the timer.
+						if (timers.ContainsKey(name))
+						{
+							timers[name].SafeTimerChange(Timeout.Infinite, Timeout.Infinite, ref disposeEnded);
+						}
+					}
+				}
+			}
 
-            var dueTime = task.ExecuteDuringStartup
-                ? TimeSpan.Zero
-                : task.Interval;
+			var dueTime = task.ExecuteDuringStartup
+				? TimeSpan.Zero
+				: task.Interval;
 
-            var period = task.Interval == TimeSpan.Zero
-                ? Timeout.InfiniteTimeSpan
-                : task.Interval;
+			var period = task.Interval == TimeSpan.Zero
+				? Timeout.InfiniteTimeSpan
+				: task.Interval;
 
-            // Don't start the timer before it has been added to the dictionary.
-            var timer = new Timer(
-                callback: Callback,
-                state: null,
-                dueTime: Timeout.Infinite,
-                period: Timeout.Infinite);
+			// Don't start the timer before it has been added to the dictionary.
+			var timer = new Timer(
+				callback: Callback,
+				state: null,
+				dueTime: Timeout.Infinite,
+				period: Timeout.Infinite);
 
-            timers.Add(name, timer);
 
-            // Now it's ok to start the timer.
-            timer.SafeTimerChange(dueTime, period, ref disposeEnded);
-        }
+			if (timers.ContainsKey(name))
+				timers.Remove(name);
 
-        private bool disposeEnded;
-        public void Dispose()
-        {
-            if (disposeEnded)
-                return;
+			timers.Add(name, timer);
 
-            var timeout = TimeSpan.FromSeconds(1);
+			// Now it's ok to start the timer.
+			timer.SafeTimerChange(dueTime, period, ref disposeEnded);
+		}
 
-            using (var waitHandle = new ManualResetEvent(false))
-            {
-                foreach (var task in timers)
-                {
-                    // Returns false on second dispose
-                    if (task.Value.Dispose(waitHandle))
-                    {
-                        if (!waitHandle.WaitOne(timeout))
-                        {
-                            throw new TimeoutException($"UNLEASH: Timeout waiting for task '{task.Key}' to stop..");
-                        }
-                    }
-                }
-            }
+		private bool disposeEnded;
+		public void Dispose()
+		{
+			if (disposeEnded)
+				return;
 
-            disposeEnded = true;
-            timers.Clear();
-        }
-    }
+			var timeout = TimeSpan.FromSeconds(1);
+
+			using (var waitHandle = new ManualResetEvent(false))
+			{
+				foreach (var task in timers)
+				{
+					// Returns false on second dispose
+					if (task.Value.Dispose(waitHandle))
+					{
+						if (!waitHandle.WaitOne(timeout))
+						{
+							throw new TimeoutException($"UNLEASH: Timeout waiting for task '{task.Key}' to stop..");
+						}
+					}
+				}
+			}
+
+			disposeEnded = true;
+			timers.Clear();
+		}
+	}
 }
--- tests/Unleash.Tests/ClientFactory/SyncStartupUnitTest.cs
+++ tests/Unleash.Tests/ClientFactory/SyncStartupUnitTest.cs
@@ -1,205 +1,219 @@
-﻿using NUnit.Framework;
-using NUnit.Framework.Internal;
-using Unleash.Communication;
-using FakeItEasy;
-using Unleash.ClientFactory;
+﻿using FakeItEasy;
 using FluentAssertions;
-using static Unleash.Tests.Specifications.TestFactory;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
 using System.Net.Http.Headers;
 using System.Text;
+using Unleash.ClientFactory;
+using Unleash.Communication;
 using Unleash.Scheduling;
+using static Unleash.Tests.Specifications.TestFactory;
 
 namespace Unleash.Tests.ClientFactory
 {
-    public class SyncStartupUnitTest
-    {
-        private IUnleashApiClient mockApiClient { get; set; }
-        private UnleashSettings settings { get; set; }
-        private IUnleashClientFactory unleashFactory { get; set; }
+	public class SyncStartupUnitTest
+	{
+		private IUnleashApiClient mockApiClient { get; set; }
+		private UnleashSettings settings { get; set; }
+		private IUnleashClientFactory unleashFactory { get; set; }
 
-        [SetUp]
-        public void Setup()
-        {
-            mockApiClient = A.Fake<IUnleashApiClient>();
-            settings = new MockedUnleashSettings(instanceTag: "test instance SyncStartupUnitTest");
-            unleashFactory = new UnleashClientFactory();
-        }
+		[SetUp]
+		public void Setup()
+		{
+			mockApiClient = A.Fake<IUnleashApiClient>();
+			settings = new MockedUnleashSettings(instanceTag: "test instance SyncStartupUnitTest");
+			unleashFactory = new UnleashClientFactory();
+		}
 
-        [Test(Description = "Immediate initialization: Should only fetch toggles once")]
-        public async Task ImmediateInitializationFetchCount()
-        {
-            settings.UnleashApiClient = mockApiClient;
+		[Test(Description = "Immediate initialization: Should only fetch toggles once")]
+		public async Task ImmediateInitializationFetchCount()
+		{
+			settings.UnleashApiClient = mockApiClient;
 
-            var unleash = await unleashFactory.CreateClientAsync(settings, synchronousInitialization: true);
+			var unleash = await unleashFactory.CreateClientAsync(settings, synchronousInitialization: true);
 
-            A.CallTo(() => mockApiClient.FetchToggles(string.Empty, A<CancellationToken>.Ignored, true))
-                .MustHaveHappenedOnceExactly();
-        }
-
-        [Test(Description = "Immediate initialization: Should be ready after creation")]
-        public async Task ImmediateInitializationReadyAfterConstruction()
-        {
-            var unleash = await unleashFactory.CreateClientAsync(settings, synchronousInitialization: true);
-
-            unleash.IsEnabled("one-enabled", false)
-                .Should().BeTrue();
-        }
-
-        [Test(Description = "Immediate initialization: Should bubble up errors")]
-        public void ImmediateInitializationBubbleErrors()
-        {
-            settings.UnleashApiClient = mockApiClient;
-            A.CallTo(() => mockApiClient.FetchToggles(A<string>.Ignored, A<CancellationToken>.Ignored, true))
-                .Throws<Exception>();
-
-            Assert.ThrowsAsync<Exception>(async () => await unleashFactory.CreateClientAsync(settings, synchronousInitialization: true));
-        }
-
-        [Test(Description = "Immediate initialization: Should bubble up async fetch errors")]
-        public void ImmediateInitializationBubbleAsyncErrors()
-        {
-            settings.UnleashApiClient = mockApiClient;
-            A.CallTo(() => mockApiClient.FetchToggles(A<string>.Ignored, A<CancellationToken>.Ignored, true))
-                .ThrowsAsync(new Exception());
-
-            Assert.ThrowsAsync<Exception>(async () => await unleashFactory.CreateClientAsync(settings, synchronousInitialization: true));
-        }
-
-        [Test(Description = "Delayed initialization: Should be ready after creation")]
-        public void DelayedInitializationNotReadyAfterConstruction()
-        {
-            var unleash = unleashFactory.CreateClientAsync(settings).Result;
-
-            unleash.IsEnabled("one-enabled", false)
-                .Should().BeFalse();
-        }
-
-        [Test]
-        public void Synchronous_Initialization_400s_Throws()
-        {
-            // Act, Assert
-            Assert.Throws<UnleashException>(() =>
-            {
-                var unleash = GetUnleash(new HttpResponseMessage()
-                {
-                    StatusCode = System.Net.HttpStatusCode.Forbidden,
-                    Headers =
-                    {
-                        ETag = new EntityTagHeaderValue("\"123\"")
-                    }
-                });
-            });
-        }
-
-        [Test]
-        public void Synchronous_Initialization_429_Throws()
-        {
-            // Act, Assert
-            Assert.Throws<UnleashException>(() =>
-            {
-                var unleash = GetUnleash(new HttpResponseMessage()
-                {
-                    StatusCode = System.Net.HttpStatusCode.Forbidden,
-                    Headers =
-                    {
-                        ETag = new EntityTagHeaderValue("\"123\"")
-                    }
-                });
-            });
-        }
-
-        [Test]
-        public void Synchronous_Initialization_304_Does_Not_Throw()
-        {
-            // Act, Assert
-            Assert.DoesNotThrow(() =>
-            {
-                var unleash = GetUnleash(new HttpResponseMessage()
-                {
-                    StatusCode = System.Net.HttpStatusCode.NotModified,
-                    Headers =
-                    {
-                        ETag = new EntityTagHeaderValue("\"123\"")
-                    }
-                });
-            });
-        }
+			A.CallTo(() => mockApiClient.FetchToggles(string.Empty, A<CancellationToken>.Ignored, true))
+				.MustHaveHappenedOnceExactly();
+		}
 
 
-        [Test]
-        public void Synchronous_Initialization_Ok_Does_Not_Throw()
-        {
-            // Act, Assert
-            Assert.DoesNotThrow(() =>
-            {
-                var unleash = GetUnleash(new HttpResponseMessage()
-                {
-                    StatusCode = System.Net.HttpStatusCode.OK,
-                    Content = new StringContent(TestData, Encoding.UTF8, "application/json"),
-                    Headers =
-                    {
-                        ETag = new EntityTagHeaderValue("\"123\"")
-                    }
-                });
-            });
-        }
+		[Test(Description = "Immediate initialization: Should fetch toggles for each client instance")]
+		public async Task ImmediateInitializationFetchCountForMultipleClients()
+		{
+			settings.UnleashApiClient = mockApiClient;
 
-        [Test]
-        public void Synchronous_Initialization_302_Throws()
-        {
-            // Act, Assert
-            Assert.Throws<UnleashException>(() =>
-            {
-                var unleash = GetUnleash(new HttpResponseMessage()
-                {
-                    StatusCode = System.Net.HttpStatusCode.Found,
-                    Headers =
-                    {
-                        ETag = new EntityTagHeaderValue("\"123\"")
-                    }
-                });
-            });
-        }
+			var unleash1 = await unleashFactory.CreateClientAsync(settings, synchronousInitialization: true);
+			var unleash2 = await unleashFactory.CreateClientAsync(settings, synchronousInitialization: true);
 
-        [Test]
-        public void Synchronous_Initialization_500_Throws()
-        {
-            // Act, Assert
-            Assert.Throws<UnleashException>(() =>
-            {
-                var unleash = GetUnleash(new HttpResponseMessage()
-                {
-                    StatusCode = System.Net.HttpStatusCode.InternalServerError,
-                    Headers =
-                    {
-                        ETag = new EntityTagHeaderValue("\"123\"")
-                    }
-                });
-            });
-        }
+			A.CallTo(() => mockApiClient.FetchToggles(string.Empty, A<CancellationToken>.Ignored, true))
+				.MustHaveHappenedTwiceExactly();
+		}
 
-        private IUnleash GetUnleash(HttpResponseMessage response)
-        {
-            var fakeHttpClientFactory = A.Fake<IHttpClientFactory>();
-            var settings = new UnleashSettings()
-            {
-                AppName = "testapp",
-                UnleashApi = new Uri("http://localhost:8080/"),
-                ScheduledTaskManager = A.Fake<IUnleashScheduledTaskManager>(),
-                HttpClientFactory = fakeHttpClientFactory,
-                DisableSingletonWarning = true
-            };
-            var responseContent = TestData;
-            var fakeHttpMessageHandler = new TestHttpMessageHandler();
-            fakeHttpMessageHandler.Response = response;
-            var client = new HttpClient(fakeHttpMessageHandler);
-            client.BaseAddress = settings.UnleashApi;
-            var factory = new UnleashClientFactory();
-            A.CallTo(() => fakeHttpClientFactory.Create(A<Uri>._)).Returns(client);
-            return factory.CreateClient(settings, synchronousInitialization: true);
-        }
 
-        public string TestData => @"{
+		[Test(Description = "Immediate initialization: Should be ready after creation")]
+		public async Task ImmediateInitializationReadyAfterConstruction()
+		{
+			var unleash = await unleashFactory.CreateClientAsync(settings, synchronousInitialization: true);
+
+			unleash.IsEnabled("one-enabled", false)
+				.Should().BeTrue();
+		}
+
+		[Test(Description = "Immediate initialization: Should bubble up errors")]
+		public void ImmediateInitializationBubbleErrors()
+		{
+			settings.UnleashApiClient = mockApiClient;
+			A.CallTo(() => mockApiClient.FetchToggles(A<string>.Ignored, A<CancellationToken>.Ignored, true))
+				.Throws<Exception>();
+
+			Assert.ThrowsAsync<Exception>(async () => await unleashFactory.CreateClientAsync(settings, synchronousInitialization: true));
+		}
+
+		[Test(Description = "Immediate initialization: Should bubble up async fetch errors")]
+		public void ImmediateInitializationBubbleAsyncErrors()
+		{
+			settings.UnleashApiClient = mockApiClient;
+			A.CallTo(() => mockApiClient.FetchToggles(A<string>.Ignored, A<CancellationToken>.Ignored, true))
+				.ThrowsAsync(new Exception());
+
+			Assert.ThrowsAsync<Exception>(async () => await unleashFactory.CreateClientAsync(settings, synchronousInitialization: true));
+		}
+
+		[Test(Description = "Delayed initialization: Should be ready after creation")]
+		public void DelayedInitializationNotReadyAfterConstruction()
+		{
+			var unleash = unleashFactory.CreateClientAsync(settings).Result;
+
+			unleash.IsEnabled("one-enabled", false)
+				.Should().BeFalse();
+		}
+
+		[Test]
+		public void Synchronous_Initialization_400s_Throws()
+		{
+			// Act, Assert
+			Assert.Throws<UnleashException>(() =>
+			{
+				var unleash = GetUnleash(new HttpResponseMessage()
+				{
+					StatusCode = System.Net.HttpStatusCode.Forbidden,
+					Headers =
+					{
+						ETag = new EntityTagHeaderValue("\"123\"")
+					}
+				});
+			});
+		}
+
+		[Test]
+		public void Synchronous_Initialization_429_Throws()
+		{
+			// Act, Assert
+			Assert.Throws<UnleashException>(() =>
+			{
+				var unleash = GetUnleash(new HttpResponseMessage()
+				{
+					StatusCode = System.Net.HttpStatusCode.Forbidden,
+					Headers =
+					{
+						ETag = new EntityTagHeaderValue("\"123\"")
+					}
+				});
+			});
+		}
+
+		[Test]
+		public void Synchronous_Initialization_304_Does_Not_Throw()
+		{
+			// Act, Assert
+			Assert.DoesNotThrow(() =>
+			{
+				var unleash = GetUnleash(new HttpResponseMessage()
+				{
+					StatusCode = System.Net.HttpStatusCode.NotModified,
+					Headers =
+					{
+						ETag = new EntityTagHeaderValue("\"123\"")
+					}
+				});
+			});
+		}
+
+
+		[Test]
+		public void Synchronous_Initialization_Ok_Does_Not_Throw()
+		{
+			// Act, Assert
+			Assert.DoesNotThrow(() =>
+			{
+				var unleash = GetUnleash(new HttpResponseMessage()
+				{
+					StatusCode = System.Net.HttpStatusCode.OK,
+					Content = new StringContent(TestData, Encoding.UTF8, "application/json"),
+					Headers =
+					{
+						ETag = new EntityTagHeaderValue("\"123\"")
+					}
+				});
+			});
+		}
+
+		[Test]
+		public void Synchronous_Initialization_302_Throws()
+		{
+			// Act, Assert
+			Assert.Throws<UnleashException>(() =>
+			{
+				var unleash = GetUnleash(new HttpResponseMessage()
+				{
+					StatusCode = System.Net.HttpStatusCode.Found,
+					Headers =
+					{
+						ETag = new EntityTagHeaderValue("\"123\"")
+					}
+				});
+			});
+		}
+
+		[Test]
+		public void Synchronous_Initialization_500_Throws()
+		{
+			// Act, Assert
+			Assert.Throws<UnleashException>(() =>
+			{
+				var unleash = GetUnleash(new HttpResponseMessage()
+				{
+					StatusCode = System.Net.HttpStatusCode.InternalServerError,
+					Headers =
+					{
+						ETag = new EntityTagHeaderValue("\"123\"")
+					}
+				});
+			});
+		}
+
+		private IUnleash GetUnleash(HttpResponseMessage response)
+		{
+			var fakeHttpClientFactory = A.Fake<IHttpClientFactory>();
+			var settings = new UnleashSettings()
+			{
+				AppName = "testapp",
+				UnleashApi = new Uri("http://localhost:8080/"),
+				ScheduledTaskManager = A.Fake<IUnleashScheduledTaskManager>(),
+				HttpClientFactory = fakeHttpClientFactory,
+				DisableSingletonWarning = true
+			};
+			var responseContent = TestData;
+			var fakeHttpMessageHandler = new TestHttpMessageHandler();
+			fakeHttpMessageHandler.Response = response;
+			var client = new HttpClient(fakeHttpMessageHandler);
+			client.BaseAddress = settings.UnleashApi;
+			var factory = new UnleashClientFactory();
+			A.CallTo(() => fakeHttpClientFactory.Create(A<Uri>._)).Returns(client);
+			return factory.CreateClient(settings, synchronousInitialization: true);
+		}
+
+		public string TestData => @"{
             ""version"": 2,
             ""features"": [
                 {
@@ -239,5 +253,5 @@ namespace Unleash.Tests.ClientFactory
                 }
             ]
         }";
-    }
+	}
 }
