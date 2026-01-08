6f744fd0f3f0d2ba943dbcc6679e8cbc8f2fb986
David Leek <david@getunleash.io>
2026-01-08 14:54:37 +0100

    chore: XMLDoc update for client factory

--- src/Unleash/ClientFactory/UnleashClientFactory.cs
+++ src/Unleash/ClientFactory/UnleashClientFactory.cs
@@ -18,7 +18,9 @@ namespace Unleash.ClientFactory
         /// <summary>
         /// Initializes a new instance of Unleash client.
         /// </summary>
+        /// <param name="settings">Unleash settings.</param>
         /// <param name="synchronousInitialization">If true, fetch and cache toggles before returning. If false, allow the unleash client schedule an initial poll of features in the background</param>
+        /// <param name="callback">Subscribe to Unleash events by adding a callback that accepts an EventCallbackConfig object.</param>
         /// <param name="strategies">Custom strategies, added in addtion to builtIn strategies.</param>
         public IUnleash CreateClient(UnleashSettings settings, bool synchronousInitialization = false, Action<EventCallbackConfig> callback = null, params IStrategy[] strategies)
         {
@@ -50,7 +52,9 @@ namespace Unleash.ClientFactory
         /// <summary>
         /// Initializes a new instance of Unleash client.
         /// </summary>
+        /// <param name="settings">Unleash settings.</param>
         /// <param name="synchronousInitialization">If true, fetch and cache toggles before returning. If false, allow the unleash client schedule an initial poll of features in the background</param>
+        /// <param name="callback">Subscribe to Unleash events by adding a callback that accepts an EventCallbackConfig object.</param>
         /// <param name="strategies">Custom strategies, added in addtion to builtIn strategies.</param>
         public async Task<IUnleash> CreateClientAsync(UnleashSettings settings, bool synchronousInitialization = false, Action<EventCallbackConfig> callback = null, params IStrategy[] strategies)
         {
